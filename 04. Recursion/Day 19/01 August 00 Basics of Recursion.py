8bc9b7835781cca4fba0c3f7689455669d7a0e83
Sachin Madhukar <sachinkumarmadhukar@gmail.com>
2021-08-03 08:08:46 +0530

    Basics of Recursion

--- 04. Recursion/Day 19/01 August 00 Basics of Recursion.py
+++ 04. Recursion/Day 19/01 August 00 Basics of Recursion.py
@@ -1,59 +1,59 @@
-# def fun():
-#     print("Sachin")
-#     fun()
-# print(fun())
+def fun():
+    print("Sachin")
+    fun()
+print(fun())
 
 
 # Print Your name for n times using Recursion
 
 # 1
 
-# def fun(n):
-#     if n<=0:    
-#         # --- Base Case
-#         return
-#     print("Sachin")
-#     fun(n-1)
-# n=int(input())
-# print(fun(n))
+def fun(n):
+    if n<=0:    
+        # --- Base Case
+        return
+    print("Sachin")
+    fun(n-1)
+n=int(input())
+print(fun(n))
 
 # 2
 
-# def fun(n):
-#     if n==0:
-#         return 
-#     print(n)
-#     fun(n-1)
-#     print(n)
-# print(fun(3))
+def fun(n):
+    if n==0:
+        return 
+    print(n)
+    fun(n-1)
+    print(n)
+print(fun(3))
 
 # 3
 
-# def fun(n):
-#     if n==0:
-#         return 
-#     fun(n-1)
-#     print(n)
-#     fun(n-1)
-# print(fun(3))
+def fun(n):
+    if n==0:
+        return 
+    fun(n-1)
+    print(n)
+    fun(n-1)
+print(fun(3))
 
 # 4
 
-# def fun(n):
-#     if n<=1:
-#         return 0
-#     else:
-#         return 1+fun(n/2)
-# print(fun(16))
+def fun(n):
+    if n<=1:
+        return 0
+    else:
+        return 1+fun(n/2)
+print(fun(16))
 
 # 5
 
-# def fun(n):
-#     if n<=1:
-#         return 0
-#     else:
-#         return 1+fun(n/2)
-# print(fun(8))
+def fun(n):
+    if n<=1:
+        return 0
+    else:
+        return 1+fun(n/2)
+print(fun(8))
 
 # 6
 
